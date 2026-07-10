e2a927654b70e6c6116f719896dca7df8b09638d
hrxaudio <hrxaudio@gmail.com>
2026-07-11 01:56:02 +0700

    Add service worker registration to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // HRX AudioLab Main Script
 
 console.log("HRX AudioLab Loaded");
+
+// Register Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("sw.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch(err => console.error("Service Worker Failed:", err));
+  });
+}
