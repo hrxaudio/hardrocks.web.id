a09373dc654c15e1d8be1e85444706f54be97ac1
hrxaudio <hrxaudio@gmail.com>
2026-07-11 01:40:52 +0700

    Add random video selection for background

--- js/background.js
+++ js/background.js
@@ -1,1 +1,14 @@
+const videos=[
+"epJU25FJqSQ",
+"MbtBbdLSs6I",
+"emUQ9CQAtwE",
+"dxSghamkPho",
+"4XJePuR1C9I",
+"4aI0jdtU0w4",
+"NIPV_5ItCYw"
+];
 
+const random=videos[Math.floor(Math.random()*videos.length)];
+
+document.getElementById("bgvideo").src=
+`https://www.youtube.com/embed/${random}?autoplay=1&mute=1&loop=1&playlist=${random}&controls=0&rel=0&modestbranding=1&playsinline=1`;
